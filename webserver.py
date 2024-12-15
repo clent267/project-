6772ae7e9e03ac29ffc43691d77bc660101c9c87
clent267 <138356434+clent267@users.noreply.github.com>
2024-12-15 09:49:02 +0800

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,22 @@
+from flask import Flask,request
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive no cap"
+
+@app.route('/send-visit', methods=['GET','POST'])
+def visit_embed():
+    return visit()
+
+@app.route('/send-result', methods=['GET','POST'])
+def result_embed():
+    return result()
+
+def keep_alive():
+    t = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
+    t.start()
+
+if __name__ == "__main__":
+    keep_alive()
